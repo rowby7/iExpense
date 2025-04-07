72110a3af829d62a4c8c5f2dd3736bc89d81b88a
rowby7 <rowby_villanueva@outlook.com>
2025-04-07 16:28:39 -0400

    2nd commit

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Rowby Villanueva on 4/7/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
